5fb4f3de8e9c2386248b39fe37ecf50d93059d95
RicardoCostaDev001 <ricardocunhacosta@gmail.com>
2022-08-26 14:16:14 -0400

    teste commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const routes = require('./routes')
 const app = express();
 
 app.use(express.json());
+
 app.use(routes);
 
 app.listen(8585, () => console.log('Server started at http://localhost:8585'));
